5750b462d5bcd8a404966168a117f112a15c5a7f
abdallah <abdallhamohamed116@gmail.com>
2023-12-15 14:09:03 +0200

    products model in data layer

---
+++ lib/Products/data/model/products_model.dart
@@ -0,0 +1,30 @@
+import 'package:slash_task/Products/domain/entity/prodcts_entity.dart';
+
+class ProductsModel extends ProductsEntity {
+  ProductsModel(
+      {required super.id,
+      required super.title,
+      required super.description,
+      required super.price,
+      required super.discountPercentage,
+      required super.rating,
+      required super.stock,
+      required super.brand,
+      required super.category,
+      required super.thumbnail,
+      required super.images});
+  factory ProductsModel.formJson(Map<String, dynamic> json) {
+    return ProductsModel(
+        id: json['id'],
+        title: json['title'],
+        description: json['description'],
+        price: json['price'],
+        discountPercentage: json['discountPercentage'],
+        rating: json['rating'],
+        stock: json['stock'],
+        brand: json['brand'],
+        category: json['category'],
+        thumbnail: json['thumbnail'],
+        images: json['images']);
+  }
+}
